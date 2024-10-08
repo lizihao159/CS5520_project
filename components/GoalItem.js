87cca5050bfcd933fe57191f5d3cd27d34e10f37
lizihao159 <lizihao159@gmail.com>
2024-10-08 14:04:58 -0700

    add a ripple effect that only valid on android device

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native'; // Import useNavigation hook
 
 const GoalItem = ({ goal, onDelete }) => {
+  const [isPressed, setIsPressed] = useState(false); // Track pressed state
   const navigation = useNavigation(); // Get the navigation object
 
   // Function to handle navigation to GoalDetails screen
@@ -11,8 +12,13 @@ const GoalItem = ({ goal, onDelete }) => {
   };
 
   return (
-    <Pressable onPress={handlePress}>
-      <View style={styles.textWrapper}>
+    <Pressable
+      onPressIn={() => setIsPressed(true)}  // Set pressed state on press
+      onPressOut={() => setIsPressed(false)} // Reset state on release
+      onPress={handlePress}
+      android_ripple={{borderless: false, foreground: false }} // Adding ripple effect for Android
+    >
+      <View style={isPressed ? styles.ripplestyle : styles.textWrapper}>
         <Text style={styles.goalText}>{goal.text}</Text>
         {/* Delete button */}
         <Button title="X" color="black" onPress={() => onDelete(goal.id)} />
@@ -24,7 +30,7 @@ const GoalItem = ({ goal, onDelete }) => {
 const styles = StyleSheet.create({
   textWrapper: {
     backgroundColor: '#d3d3d3',
-    padding: 10,
+    padding: 15,
     borderRadius: 6,
     marginVertical: 10,
     flexDirection: 'row',
@@ -34,6 +40,15 @@ const styles = StyleSheet.create({
   goalText: {
     fontSize: 20,
     color: 'blue',
+  },
+  ripplestyle: {
+    backgroundColor: 'blue',  // Light blue background when pressed
+    padding: 15,
+    borderRadius: 6,
+    marginVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 });
 
