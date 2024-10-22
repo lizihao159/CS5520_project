db90d9aa8b1d691dd44b261e0a84dc513446a185
lizihao159 <lizihao159@gmail.com>
2024-10-22 13:56:50 -0700

    update style of header to make it have dynamic size when rotate

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,24 @@
-// components/Header.js
-
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, useWindowDimensions } from 'react-native';
 import React from 'react';
 
 export default function Header({ name, headerStyle, textStyle }) {
+    const { width, height } = useWindowDimensions(); // Hook to get current width and height
+
+    // Define dynamic padding based on the height and width of the screen
+    const paddingVerticalDynamic = height < 415 ? 0 : 10;
+    const paddingHorizontalDynamic = width < 380 ? 10 : 20;
+
     return (
-        <View style={[styles.headerContainer, headerStyle]}>
+        <View
+            style={[
+                styles.headerContainer,
+                headerStyle,
+                { 
+                    paddingVertical: paddingVerticalDynamic, 
+                    paddingHorizontal: paddingHorizontalDynamic 
+                }
+            ]}
+        >
             <Text style={[styles.headerText, textStyle]}>{name}</Text>
         </View>
     );
@@ -15,14 +28,15 @@ const styles = StyleSheet.create({
     headerContainer: {
         borderWidth: 2,
         borderColor: 'purple',
-        padding: 7,
         borderRadius: 10,
-        marginBottom: 10, 
+        marginBottom: 10,
+        alignSelf: 'center',
+        backgroundColor: '#e1bee7', // Light purple background
     },
     headerText: {
         color: 'purple',
-        fontSize: 20, 
-        fontWeight: 'flat', 
-        textAlign: 'center', 
+        fontSize: 22,
+        fontWeight: '600',
+        textAlign: 'center',
     },
 });
